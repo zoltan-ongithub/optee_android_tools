6e74c47b5e1e0e50d5df79761055e0113aed82ad
Zoltan Kuscsik <zoltan.kuscsik@linaro.org>
2016-04-08 09:18:33 +0200

    Initial commit

---
+++ build_ta.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+COMPILER_PATH=$PWD/gcc-linaro-4.9-2015.05-x86_64_aarch64-linux-gnu/bin
+CURRENT_BUILD_PATH=$(dirname $(realpath $0))
+
+NUMBER_OF_CPU_CORES=`grep -c ^processor /proc/cpuinfo`
+OPTEE_OS_DIR="optee_os"
+
+export PATH=COMPILER_PATH:$PATH
+
+set_optee_os_hikey_vars() {
+  export PLATFORM=hikey
+  export CROSS_COMPILE=aarch64-linux-gnu-
+  ARM64_core=y
+  TA_TARGETS=ta_arm64
+  OPTEE_OS_DEV_KIT_PATH="out/arm-plat-hikey/export-ta_arm64"
+}
+
+if [ -z "$ANDROID_BUILD_TOP" ]; then
+   echo "Error: Before running this script you should call in the main Android directory"
+   echo
+   echo "  $ source build/envsetup.sh "
+   echo "  $ lunch <whatever> "
+   exit 1
+fi
+
+###########################################################
+# Select target platform
+###########################################################
+if [ "$1" == "hikey" ]; then
+  set_optee_os_hikey_vars
+else
+  echo "Missing platform argument from ./$0 <platform> <ta_project_list_file>"
+  echo "Valid arguments:"
+  echo "  hikey "
+  exit 1
+fi
+
+if  [ -z "$2" ]; then
+  echo "Missing TA list from ./$0 <platform> <ta_project_list_file> "
+  exit 1
+fi
+
+TA_PROJECT_LIST_SOURCE="$2"
+
+if [ -f $TA_PROJECT_LIST_SOURCE ]; then
+  source $TA_PROJECT_LIST_SOURCE
+else
+  echo "ERROR: File $TA_PROJECT_LIST_SOURCE does not exists"
+  exit 1
+fi
+
+
+if [ -z "$ANDROID_OPTEE_PROJECT_LIST" ]; then
+  echo "TA projects list file must define the ANDROID_OPTEE_PROJECT_LIST variable."
+  exit 1
+fi
+
+##########################################################
+# Build OPTEE OS
+##########################################################
+make  -C $CURRENT_BUILD_PATH/$OPTEE_OS_DIR -j$NUMBER_OF_CPU_CORES \
+      ta-targets=$TA_TARGETS \
+      CFG_ARM64_core=$ARM64_core
+
+##########################################################
+# Build trusted applications
+##########################################################
+
+export TA_DEV_KIT_DIR=$CURRENT_BUILD_PATH/$OPTEE_OS_DIR/$OPTEE_OS_DEV_KIT_PATH
+
+for ta_target in $ANDROID_OPTEE_PROJECT_LIST
+do
+    make  -C $ANDROID_BUILD_TOP/$ta_target
+    android_mk="$ANDROID_BUILD_TOP/$ta_target/Android.mk"
+    if [ ! -f "$android_mk" ]; then
+      echo "ERROR: Android.mk does not exists in $ANDROID_BUILD_TOP/$ta_target"
+      echo "       TA is built but it wont be included in the system.img"
+      exit 1
+    fi
+done
+
